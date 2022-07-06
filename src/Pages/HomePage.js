c1a32293958b3cb15253eccf2cc4d2ba9e0e7865
BHANUTEJA SANGARS <88140271+Bhanutejasangars@users.noreply.github.com>
2022-07-06 10:56:34 +0530

    code updated

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomePage.css";
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>
+        Welcome to <span className="company-name">SAFAR-BUS</span>
+      </h1>
+      <h2>
+        Worry less,<span className="company-name">Travel More</span>
+      </h2>
+    </div>
+  );
+}
+
+export default HomePage;
